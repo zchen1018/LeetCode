c52d0e727a6ac91422a5732551e6e3c66612106e
Zhuangye Chen <zhuangye@stat.wisc.edu>
2016-04-19 23:09:59 -0500

    now 6

--- Algorithms/easy1-10.py
+++ Algorithms/easy1-10.py
@@ -113,4 +113,39 @@ class Solution(object):
 #     26 -> Z
 #     27 -> AA
 #     28 -> AB
+def convertToTitle(n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        res = ''
+        base = ord('A')
+        while n:
+            n, r = divmod(n - 1, 26)
+            res = '{}{}'.format(chr(base + r), res)
+        return res
+
+
+
+# 171. Excel Sheet Column Number
+# Given a column title as appear in an Excel sheet, return its corresponding column number.
+# For example:
+#     A -> 1
+#     B -> 2
+#     C -> 3
+#     ...
+#     Z -> 26
+#     AA -> 27
+#     AB -> 28 
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        num = 0
+        for i in range(len(s)):
+            temp = ord(s[len(s) - i -1]) - 64
+            num += temp * 26**i
+        return num
 
