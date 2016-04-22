6ac1783187b06d254f1d43a0a23021e57bc32711
Zhuangye Chen <zhuangye@stat.wisc.edu>
2016-04-22 16:37:54 -0500

    7 now

--- Algorithms/easy11-20.py
+++ Algorithms/easy11-20.py
@@ -85,6 +85,8 @@ class Solution(object):
         return n == 1
 
         # without loop
+        # For a 32-bit int, the maximal value is Maxint = 2^32/2 - 1
+        # Thus, the maximal power of 3 is floor(log_3^Maxint) = 19
         # return n > 0 and 3**19 % n == 0
 
 
@@ -117,3 +119,35 @@ class Solution(object):
             
         else:
             return True
+
+
+
+# 231. Power of Two
+# Given an integer, write a function to determine if it is a power of two.
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        max_power = math.floor(math.log(2**32/2 -1, 2))
+        return n > 0 and 2**max_power % n == 0
+
+
+
+# 121. Best Time to Buy and Sell Stock
+# Say you have an array for which the ith element is the price of a given stock on day i.
+# If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price, max_profit = float('inf'), 0
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(profit, max_profit)
+            
+        return max_profit
