cfea510fd58b0545a36688cdb2597973f6556deb
Zhuangye Chen <zhuangye@stat.wisc.edu>
2016-04-26 13:46:14 -0500

    3rd 10

--- Algorithms/easy21-30.py
+++ Algorithms/easy21-30.py
@@ -162,3 +162,110 @@ class Solution(object):
 
 
 
+# 235. Lowest Common Ancestor of a Binary Search Tree
+# Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+#         _______6______
+#        /              \
+#     ___2__          ___8__
+#    /      \        /      \
+#    0      _4       7       9
+#          /  \
+#          3   5
+# For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        while root:
+            if max(p.val, q.val) < root.val:
+                root = root.left
+            elif min(p.val, q.val) > root.val:
+                root = root.right
+            else: # root.val equals to one of p.val and q.val or root.val is in the middle of the two
+                return root
+
+
+
+# 236. Lowest Common Ancestor of a Binary Tree
+# Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+
+#         _______3______
+#        /              \
+#     ___5__          ___1__
+#    /      \        /      \
+#    6      _2       0       8
+#          /  \
+#          7   4
+# For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root in (None, p, q): return root
+        left, right = (self.lowestCommonAncestor(kid, p, q)
+                   for kid in (root.left, root.right))
+        return root if left and right else left or right
+
+
+
+# 101. Symmetric Tree
+# Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+# For example, this binary tree is symmetric:
+#     1
+#    / \
+#   2   2
+#  / \ / \
+# 3  4 4  3
+# But the following is not:
+#     1
+#    / \
+#   2   2
+#    \   \
+#    3    3
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        # recursively
+        if not root: return True
+        return self.isMirror(root.left, root.right)
+        
+    
+    def isMirror(self, node1, node2):
+        if not node1 and not node2: return True
+        elif not node1 or not node2: return False
+        
+        return node1.val == node2.val and self.isMirror(node1.left, node2.right) and self.isMirror(node1.right, node2.left)
